40d711f39816786b77f7a946086d9e4f33d6b6db
taravann <heng.t.taravann@gmail.com>
2023-05-03 22:42:29 +1000

    refactor: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
-
 import WordCounter from "./components/word-counter/index.component";
 import WordDisplay from "./components/word-display/index.component";
 import Header from "./components/header/index.component";
 import { CurrentLetterIndexContextProvider } from "./contexts/current-letter-index-context";
 import { WordCounterProvider } from "./contexts/word-counter-context";
 
-// interface Letter {
-//   lastLetter: boolean;
-//   character: string;
-// }
-
 function App() {
-  const [wordCounter, setWordCounter] = useState<number>(0);
-
   return (
     <CurrentLetterIndexContextProvider>
       <WordCounterProvider>
