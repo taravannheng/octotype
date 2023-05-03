bf1688c3a970c984352ac2b2ce789d4ec1151356
taravann <heng.t.taravann@gmail.com>
2023-05-03 21:29:16 +1000

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import WordCounter from "./components/word-counter/index.component";
 import WordDisplay from "./components/word-display/index.component";
+import Header from "./components/header/index.component";
 import { CurrentLetterIndexContextProvider } from "./contexts/current-letter-index-context";
 import { WordCounterProvider } from "./contexts/word-counter-context";
 
@@ -16,9 +17,10 @@ function App() {
   return (
     <CurrentLetterIndexContextProvider>
       <WordCounterProvider>
-        <div className="App p-24">
+        <div className="App">
+          <Header headerStyle="mb-24" />
           <WordCounter />
-          <WordDisplay numWords={200} />
+          <WordDisplay numWords={200} wordDisplayStyle="px-12 sm:px-24" />
         </div>
       </WordCounterProvider>
     </CurrentLetterIndexContextProvider>
---
+++ src/components/header/index.component.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+import Logo from '../../assets/logos/logo.png'
+
+interface HeaderProps {
+  headerStyle?: string;
+}
+
+const Header: FC<HeaderProps> = ({ headerStyle }) => {
+  return <div className={`flex justify-center align-items bg-primary ${ headerStyle }`}>
+    <img src={Logo} alt="logo" className="h-24 sm:h-24 xl:24" />
+  </div>;
+};
+
+export default Header;
