bb3065b56be2338dcbc25c5cc5670b89333c50de
taravann <heng.t.taravann@gmail.com>
2023-05-03 20:08:18 +1000

    refactor: remove key press handler function

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
-
+import { useState, useEffect } from "react";
 
 import WordCounter from "./components/word-counter/index.component";
 import WordDisplay from "./components/word-display/index.component";
+import { CurrentLetterIndexContextProvider } from "./contexts/current-letter-index-context";
+import { WordCounterProvider } from "./contexts/word-counter-context";
 
-interface Letter {
-  lastLetter: boolean;
-  character: string;
-  status: "pressed" | "unpressed";
-}
+// interface Letter {
+//   lastLetter: boolean;
+//   character: string;
+// }
 
 function App() {
   const [wordCounter, setWordCounter] = useState<number>(0);
-  const [currentLetterIndex, setCurrentLetterIndex] = useState<number>(0);
-  const [letterList, setLetterList] = useState<Letter[]>([]);
-
-  useEffect(() => {
-    
-  }, []);
-
-  // TODO: should update the event type to KeyboardEvent, however it poses an issue: KeyboardEvent is not generic
-  const handleKeyDown = (event: any) => {
-    console.log("key: " + event.key);
-    console.log("letter: " + letterList[currentLetterIndex].character);
-
-    if (
-      event.key === letterList[currentLetterIndex].character &&
-      currentLetterIndex !== letterList.length
-    ) {
-      // update key press status
-      const tempLetterList = [...letterList];
-      tempLetterList[currentLetterIndex] = {
-        ...letterList[currentLetterIndex],
-        status: "pressed",
-      };
-      setLetterList(tempLetterList);
-
-      // increment current letter index
-      setCurrentLetterIndex((prevLetterIndex) => prevLetterIndex + 1);
-
-      // update word counter
-      if (letterList[currentLetterIndex].lastLetter) {
-        setWordCounter((prevWordCounter) => prevWordCounter + 1);
-      }
-    }
-
-    // unfocus if reaches last letter
-    // if (currentLetterIndex === letterList.length - 1) {
-    //   containerRef.current!.blur();
-    // }
-  };
 
   return (
-    <div className="App p-24">
-      <WordCounter wordCount={wordCounter} />
-      <WordDisplay numWords={200} keyHandler={handleKeyDown} setLetterList={setLetterList} letterList={letterList}/>
-    </div>
+    <CurrentLetterIndexContextProvider>
+      <WordCounterProvider>
+        <div className="App p-24">
+          <WordCounter />
+          <WordDisplay numWords={200} />
+        </div>
+      </WordCounterProvider>
+    </CurrentLetterIndexContextProvider>
   );
 }
 
