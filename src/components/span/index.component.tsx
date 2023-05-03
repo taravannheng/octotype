79124eab7b68d29eeae4aa1610dd4a71f58a4066
taravann <heng.t.taravann@gmail.com>
2023-05-03 20:06:32 +1000

    feat: add function to handle key press

--- src/components/span/index.component.tsx
+++ src/components/span/index.component.tsx
@@ -1,26 +1,64 @@
-import { FC, useState, useContext, useEffect } from 'react'
+import { FC, useState, useContext, useEffect, useRef } from "react";
 
-import CurrentLetterIndexContext from '../../contexts/current-letter-index-context';
+import CurrentLetterIndexContext from "../../contexts/current-letter-index-context";
+import WordCounterContext from "../../contexts/word-counter-context";
 
 interface SpanProps {
   spanStyle: string;
   text: string;
   index: number;
+  isLastLetter: boolean;
 }
 
-const Span: FC<SpanProps> = ({ spanStyle, text, index }) => {
+const Span: FC<SpanProps> = ({ spanStyle, text, index, isLastLetter }) => {
+  const [isActive, setIsActive] = useState<boolean>(false);
   const [isPressed, setIsPressed] = useState<boolean>(false);
-  const { currentLetterIndex } = useContext(CurrentLetterIndexContext);
+  const { currentLetterIndex, setCurrentLetterIndex } = useContext(
+    CurrentLetterIndexContext
+  );
+  const { setWordCounter } = useContext(WordCounterContext);
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  // TODO: update the below event type to Keyboard Event
+  const keyHandler = (event: any) => {
+    const pressedKey = event.key;
+
+    if (pressedKey === text) {
+      setIsPressed(true);
+
+      setCurrentLetterIndex(
+        (prevCurrentLetterIndex: number) => prevCurrentLetterIndex + 1
+      );
+
+      // is last letter of the word -> update word counter
+      if (isLastLetter) {
+        setWordCounter((prevWordCounter: number) => prevWordCounter + 1);
+      }
+
+      spanRef.current!.blur();
+    }
+  };
 
   useEffect(() => {
     if (currentLetterIndex === index) {
-      setIsPressed(true);
+      setIsActive(true);
+
+      spanRef.current!.focus();
     }
-  }, [currentLetterIndex]);
+  }, [currentLetterIndex, index]);
 
   return (
-    <span className={`${spanStyle}  ${isPressed && 'text-primary'}`}>{text}</span>
-  )
-}
+    <span
+      onKeyDown={keyHandler}
+      ref={spanRef}
+      tabIndex={index}
+      className={`outline-none ${spanStyle} ${
+        isActive && "text-neutral-light"
+      }  ${isPressed && "text-primary"}`}
+    >
+      {text}
+    </span>
+  );
+};
 
-export default Span
+export default Span;
