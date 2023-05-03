89cb851fe42610e149b5bc5824b893874dd571b1
taravann <heng.t.taravann@gmail.com>
2023-05-03 20:47:33 +1000

    feat: add helper function to check if input is letter

---
+++ src/utils/helpers.ts
@@ -0,0 +1,9 @@
+import _ from "lodash";
+
+export const isLetter = (letter: any) => {
+  const convertedLetter = _.toString(letter);
+
+  // if length is larger than 1 -> the pressed key is not an alphabet
+  // e.g. if TAB key is pressed -> converted to string -> 'TAB' which is more than 1 character
+  return convertedLetter !== "" && true && convertedLetter.length === 1;
+};
