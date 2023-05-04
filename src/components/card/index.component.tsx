0ec9f2639a2f62f92d60e9db817ba34166e4a92c
taravann <heng.t.taravann@gmail.com>
2023-05-04 15:39:25 +1000

    fix: fix overflow issue in card component

--- src/components/card/index.component.tsx
+++ src/components/card/index.component.tsx
@@ -17,10 +17,10 @@ const Card: FC<CardProps> = ({
 }) => {
   return (
       <div
-        className={`card relative bg-neutral-light rounded-lg shadow-lg p-6 z-10 text-h3 flex items-center flex-col px-12 py-8 ${cardStyle}`}
+        className={`card overflow-hidden relative bg-neutral-light rounded-lg shadow-lg p-6 z-10 text-h3 flex items-center flex-col px-12 py-8 ${cardStyle}`}
       >
         <span className={`mb-1 ${titleStyle}`}>{titleText}</span> <br />{" "}
-        <span className={`text-h1 text-[80px] ${descriptionStyle}`}>
+        <span className={`text-h1 text-[80px] max-w-full truncate ${descriptionStyle}`}>
           {descriptionText}
         </span>
       </div>
