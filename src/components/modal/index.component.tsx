9f9e2d392d518af1a2b3be74979e958cdb1a791b
taravann <heng.t.taravann@gmail.com>
2023-05-08 14:09:02 +1000

    refactor: replace text and icon with children node

--- src/components/modal/index.component.tsx
+++ src/components/modal/index.component.tsx
@@ -1,18 +1,16 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 
 interface ModalProps {
   modalStyle?: string;
-  text: string;
-  logoSrc?: string;
+  children: ReactNode;
 }
 
-const Modal: FC<ModalProps> = ({ modalStyle = '', text, logoSrc }) => {
+const Modal: FC<ModalProps> = ({ modalStyle = '', children }) => {
   return (
     <div className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 w-screen h-screen ${modalStyle}`}>
       <div className="bg-neutral-light p-6 rounded-md shadow-lg w-full h-full">
         <div className={`flex justify-center items-center flex-col w-full h-full`}>
-          {logoSrc && <img src={logoSrc} alt="logo" className="h-8 mb-12" />}
-          <h1 className='text-h4 text-neutral-dark text-status-error'>{text}</h1>
+          {children}
         </div>
       </div>
     </div>
