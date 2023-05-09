d1862c582567371d6b459131306ad44736824219
taravann <heng.t.taravann@gmail.com>
2023-05-09 22:50:35 +1000

    feat: add modal to summary page

--- src/routes/summary/index.component.tsx
+++ src/routes/summary/index.component.tsx
@@ -1,10 +1,13 @@
-import { FC, useContext } from "react";
-import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { FC, useContext, useState, Fragment } from "react";
+import { faRotateRight, faInfo } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
 import Dashboard from "../../components/dashboard/index.component";
 import Header from "../../components/header/index.component";
 import Button from "../../components/button/index.component";
+import Modal from "../../components/modal/index.component";
+import Logo from '../../assets/logos/logo.png';
+import Banner from "../../components/banner/index.component";
 import TimerContext from "../../contexts/timer-context";
 import AccuracyContext from "../../contexts/accuracy-context";
 import CurrentLetterIndexContext from "../../contexts/current-letter-index-context";
@@ -18,6 +21,7 @@ interface SummaryProps {
 }
 
 const Summary: FC<SummaryProps> = ({ summaryPageStyle = "" }) => {
+  const [isBannerOpen, setIsBannerOpen] = useState(true);
   const { setTimer } = useContext(TimerContext);
   const { setAccuracy } = useContext(AccuracyContext);
   const { setCurrentLetterIndex } = useContext(CurrentLetterIndexContext);
@@ -39,22 +43,40 @@ const Summary: FC<SummaryProps> = ({ summaryPageStyle = "" }) => {
   };
 
   return (
-    <div className={`summary ${summaryPageStyle}`}>
-      <Header headerStyle="mb-24 flex items-center justify-center" />
-      <div className="summary-body px-12 sm:px-24 lg:px-64">
-        <h1 className="text-h2 flex justify-center items-center mb-12 text-neutral-light">
-          Your Stats
-        </h1>
-        <Dashboard dashboardStyle="flex justify-center items-center flex-row mb-8 px-44" />
-        <div className="button-container px-44">
-          <Button
-            label="Refresh"
-            iconSrc={faRotateRight}
-            onClick={refreshHandler}
-          />
+    <Fragment>
+      <Modal modalStyle="lg:hidden">
+        <img
+          src={Logo}
+          alt="logo"
+          className="h-[100px] md:h-[200px] mb-[96px]"
+        />
+        <Banner
+          isOpen={isBannerOpen}
+          setIsOpen={setIsBannerOpen}
+          titleText="Alert"
+          descriptionText="This application is only available for desktop only!"
+          titleTextStyle="text-status-error text-h5"
+          iconSrc={faInfo}
+          iconStyle="text-neutral-light bg-status-error"
+        />
+      </Modal>
+      <div className={`summary ${summaryPageStyle}`}>
+        <Header headerStyle="mb-24 flex items-center justify-center" />
+        <div className="summary-body px-12 sm:px-24 lg:px-64">
+          <h1 className="text-h2 flex justify-center items-center mb-12 text-neutral-light">
+            Your Stats
+          </h1>
+          <Dashboard dashboardStyle="flex justify-center items-center flex-row mb-8 px-44" />
+          <div className="button-container px-44">
+            <Button
+              label="Refresh"
+              iconSrc={faRotateRight}
+              onClick={refreshHandler}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
