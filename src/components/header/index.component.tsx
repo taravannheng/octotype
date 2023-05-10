4f493fd0aa70a36b15f96ca072b2cead80a7bd5c
taravann <heng.t.taravann@gmail.com>
2023-05-11 00:43:17 +1000

    refactor: add condition to check if first key is pressed to hide theme toggle

--- src/components/header/index.component.tsx
+++ src/components/header/index.component.tsx
@@ -1,16 +1,20 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 
 import Logo from '../../assets/logos/logo.png'
 import Toggle from "../theme-toggle/index.component";
+import FirstKeyPressedContext from "../../contexts/first-key-pressed-context";
+import { INITIAL_CONFIG } from "../../utils/constants";
 
 interface HeaderProps {
   headerStyle?: string;
 }
 
 const Header: FC<HeaderProps> = ({ headerStyle }) => {
+  const { isFirstKeyPressed } = useContext(FirstKeyPressedContext);
+
   return <div className={`flex items-center justify-between py-8 bg-transparent px-12 sm:px-24 lg:px-64 ${ headerStyle }`}>
     <img src={Logo} alt="logo" className="h-20" />
-    <Toggle />
+    {!isFirstKeyPressed && <Toggle />}
   </div>;
 };
 
