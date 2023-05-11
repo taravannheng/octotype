89c2b256c5c74ec0032d72c0730bc9ff7ec17e8e
taravann <heng.t.taravann@gmail.com>
2023-05-11 22:39:00 +1000

    feat: add label to theme toggle

--- src/components/header/index.component.tsx
+++ src/components/header/index.component.tsx
@@ -4,6 +4,7 @@ import Logo from '../../assets/logos/logo.png'
 import Toggle from "../theme-toggle/index.component";
 import FirstKeyPressedContext from "../../contexts/first-key-pressed-context";
 import TimerContext from "../../contexts/timer-context";
+import ThemeContext from "../../contexts/theme-context";
 
 interface HeaderProps {
   headerStyle?: string;
@@ -11,11 +12,12 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ headerStyle }) => {
   const { isFirstKeyPressed } = useContext(FirstKeyPressedContext);
+  const { isDarkTheme } = useContext(ThemeContext);
   const { timer } = useContext(TimerContext);
 
   return <div className={`flex items-center justify-between py-8 bg-transparent px-12 sm:px-24 lg:px-64 ${ headerStyle }`}>
     <img src={Logo} alt="logo" className="h-20" />
-    {(!isFirstKeyPressed || timer === 0) && <Toggle />}
+    {(!isFirstKeyPressed || timer === 0) && <div className="toggle-container flex flex-row gap-4 items-center"><span className={`text-body2 ${isDarkTheme ? 'text-dark-white' : 'text-light-darkest'}`}>THEME</span><Toggle /></div>}
   </div>;
 };
 
