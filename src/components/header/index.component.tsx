9eca8b821a38b22822b966df8dd5c596317bdeb2
taravann <heng.t.taravann@gmail.com>
2023-05-11 15:56:02 +1000

    refactor: make theme toggle appears when timer ends so it appears on summary page

--- src/components/header/index.component.tsx
+++ src/components/header/index.component.tsx
@@ -3,7 +3,7 @@ import { FC, useContext } from "react";
 import Logo from '../../assets/logos/logo.png'
 import Toggle from "../theme-toggle/index.component";
 import FirstKeyPressedContext from "../../contexts/first-key-pressed-context";
-import { INITIAL_CONFIG } from "../../utils/constants";
+import TimerContext from "../../contexts/timer-context";
 
 interface HeaderProps {
   headerStyle?: string;
@@ -11,10 +11,11 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ headerStyle }) => {
   const { isFirstKeyPressed } = useContext(FirstKeyPressedContext);
+  const { timer } = useContext(TimerContext);
 
   return <div className={`flex items-center justify-between py-8 bg-transparent px-12 sm:px-24 lg:px-64 ${ headerStyle }`}>
     <img src={Logo} alt="logo" className="h-20" />
-    {!isFirstKeyPressed && <Toggle />}
+    {(!isFirstKeyPressed || timer === 0) && <Toggle />}
   </div>;
 };
 
